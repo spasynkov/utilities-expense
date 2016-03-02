d46059f19fa2be1898c190e28777c15679b5bc7e
spasynkov <s.pasynkov@gmail.com>
2016-03-02 20:36:58 +0200

    added uml class diagram, creating starting tables and store data in settings file

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -8,7 +8,7 @@ public class Server {
         // проверяем, существует ли файл с настройками. если нет - запускаем процесс подготовки
         String settingsPath = "./settings";
         File settings = new File(settingsPath);
-        if (!settings.exists() && settings.isFile()) {
+        if (!settings.exists() || !settings.isFile()) {
             ServerPreparer.main(new String[] {settingsPath});
         }
     }
--- server/src/main/java/ServerPreparer.java
+++ server/src/main/java/ServerPreparer.java
@@ -14,11 +14,18 @@ public class ServerPreparer {
             settingsPath = args[0];
         } else settingsPath = "./settings";
 
-        String dbType, dbHost, dbPort, dbRootUserName, dbName, dbUserName, dbPassword;
-        StringBuilder dbConnectionString = new StringBuilder("jdbc:");
-        String mysql = "mysql", postgresql = "postgresql";
-        String driverClassName;             // сюда положим нужный драйвер в зависимости от типа БД
+        String dbType = "",
+                dbHost = "",
+                dbPort = "",
+                dbRootUserName,
+                dbName = "",
+                dbUserName = "",
+                dbPassword = "";
+        StringBuilder dbConnectionString = new StringBuilder("jdbc:");      // сюда собираем строку для подключения к базе
+        String mysql = "mysql",
+                postgresql = "postgresql";
         String noSSL = "?useSSL=false";     // исключаем предупреждение из логов; будет добавлено к строке подключения к БД последним
+        String driverClassName = "";        // сюда положим нужный драйвер в зависимости от типа БД
 
         try {
             System.out.print("Select database type. Empty string for MySQL, not empty string for PostgreSQL: ");
@@ -49,7 +56,7 @@ public class ServerPreparer {
 
             // регистрируем драйвер в зависимости от типа базы данных
             DriverManager.registerDriver((Driver) Class.forName(driverClassName).newInstance());
-            Connection connection;
+            Connection connection;  // соединение с базой
 
             System.out.print("Do you need to create new database and user? [y/n] ");
             if ("y".equalsIgnoreCase(consoleReader.readLine())) {
@@ -108,30 +115,106 @@ public class ServerPreparer {
                 System.out.print("Enter username: ");
                 dbUserName = consoleReader.readLine();
                 System.out.print("Enter password: ");
+                dbPassword = consoleReader.readLine();
 
                 connection = DriverManager.getConnection(
                         dbConnectionString.append("/").append(dbName).append(noSSL).toString(),
                         dbUserName,
-                        consoleReader.readLine());
+                        dbPassword);
             }
             System.out.println("Success!");
+
+            // Adding tables to database
+            Statement statement = connection.createStatement();
+
+            // Creating users table
+            String sqlQuery = "" +
+                    "CREATE TABLE IF NOT EXISTS `" + dbName + "`.`users` (" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT, " +
+                    "  `username` VARCHAR(45) NOT NULL, " +
+                    "  `password` VARCHAR(45) NOT NULL, " +
+                    "  `adresses` INT NOT NULL, " +
+                    "  `birthday` DATE NULL, " +
+                    "  PRIMARY KEY (`id`), " +
+                    "  UNIQUE INDEX `id_UNIQUE` (`id` ASC)) " +
+                    "ENGINE = InnoDB " +
+                    "DEFAULT CHARACTER SET = utf8;";
+            System.out.println("Table 'users' created. Edited stings: " + statement.executeUpdate(sqlQuery));
+
+            // Creating table water
+            sqlQuery = "" +
+                    "CREATE TABLE IF NOT EXISTS `" + dbName + "`.`water` (" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT, " +
+                    "  `date` DATE NULL COMMENT 'дата снятия показания счетчика', " +
+                    "  `value` INT NULL COMMENT 'значение счетчика на момент снятия', " +
+                    "  `user` INT NOT NULL COMMENT 'id пользователя', " +
+                    "  `user_adress` INT NOT NULL COMMENT 'id адреса пользователя', " +
+                    "  `created_at` DATETIME NOT NULL COMMENT 'время внесения записи в базу', " +
+                    "  PRIMARY KEY (`id`)," +
+                    "  UNIQUE INDEX `id_UNIQUE` (`id` ASC)) " +
+                    "ENGINE = InnoDB " +
+                    "DEFAULT CHARACTER SET = utf8;";
+            System.out.println("Table 'water' created. Edited stings: " + statement.executeUpdate(sqlQuery));
+
+            // Creating table electricity
+            sqlQuery = "" +
+                    "CREATE TABLE IF NOT EXISTS `" + dbName + "`.`electricity` (" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT, " +
+                    "  `date` DATE NULL COMMENT 'дата снятия показания счетчика', " +
+                    "  `value` INT NULL COMMENT 'значение счетчика на момент снятия', " +
+                    "  `date_plan` DATE NULL COMMENT 'дата прогнозного значения, выставленного в квитанции', " +
+                    "  `value_plan` INT NULL COMMENT 'прогнозное значение счетчика указанное в квитанции', " +
+                    "  `user` INT NOT NULL COMMENT 'id пользователя', " +
+                    "  `user_adress` INT NOT NULL COMMENT 'id адреса пользователя', " +
+                    "  `created_at` DATETIME NOT NULL COMMENT 'время внесения записи в базу', " +
+                    "  PRIMARY KEY (`id`)," +
+                    "  UNIQUE INDEX `id_UNIQUE` (`id` ASC)) " +
+                    "ENGINE = InnoDB " +
+                    "DEFAULT CHARACTER SET = utf8;";
+            System.out.println("Table 'electricity' created. Edited stings: " + statement.executeUpdate(sqlQuery));
+
+            // Creating table gas
+            sqlQuery = "" +
+                    "CREATE TABLE IF NOT EXISTS `" + dbName + "`.`gas` (" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT, " +
+                    "  `date` DATE NULL COMMENT 'дата снятия показания счетчика', " +
+                    "  `value` INT NULL COMMENT 'значение счетчика на момент снятия', " +
+                    "  `user` INT NOT NULL COMMENT 'id пользователя', " +
+                    "  `user_adress` INT NOT NULL COMMENT 'id адреса пользователя', " +
+                    "  `created_at` DATETIME NOT NULL COMMENT 'время внесения записи в базу', " +
+                    "  PRIMARY KEY (`id`)," +
+                    "  UNIQUE INDEX `id_UNIQUE` (`id` ASC)) " +
+                    "ENGINE = InnoDB " +
+                    "DEFAULT CHARACTER SET = utf8;";
+            System.out.println("Table 'gas' created. Edited stings: " + statement.executeUpdate(sqlQuery));
+
+            statement.close();
             connection.close();
 
         } catch (IOException | IllegalAccessException | InstantiationException | ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
-            e.printStackTrace();
+            // e.printStackTrace();
             System.err.println("Sorry, can't connect");
             System.err.println(e.getMessage());
-            System.exit(1);
+            System.exit(1);     // нет смысла продолжать. выходим.
         }
 
+        // Preparing properties
         Properties properties = new Properties();
+        properties.setProperty("dbType", dbType);
+        properties.setProperty("dbHost", dbHost);
+        properties.setProperty("dbPort", dbPort);
+        properties.setProperty("dbName", dbName);
+        properties.setProperty("dbUser", dbUserName);
+        properties.setProperty("dbPass", dbPassword);
+        properties.setProperty("JDBCDriver", driverClassName);
+        // Storing properties
         FileOutputStream outputStream;
-
         try {
             outputStream = new FileOutputStream(settingsPath);
-        } catch (FileNotFoundException e) {
+            properties.store(outputStream, null);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
